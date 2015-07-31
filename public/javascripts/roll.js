6116bd436bcdc2ba789554a70c1eeb758dcdf817
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-07-31 17:12:06 +0800

    add the javascript files about index

---
+++ public/javascripts/roll.js
@@ -0,0 +1,13 @@
+$(function() {
+    $('#slides').slidesjs({
+        width: 940,
+        height: 528,
+        navigation: false,
+        play: {
+            active: false,
+            auto: true,
+            interval: 4000,
+            swap: true
+        }
+    });
+});
