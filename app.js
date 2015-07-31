463785190874211fc82e8a021ee69414bb9439af
maming <864845434@qq.com>
2015-07-31 16:29:42 +0800

    first login page test

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var Sequelize = require('sequelize')
-var sequelize = new Sequelize('web_store', 'root', 'root', {
+var Sequelize = require('sequelize')
+var sequelize = new Sequelize('web_store', 'root', 'root', {
         host: "localhost",
-      dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
-      port:    3306, // or 5432 (for postgres)
-    })
+      dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
+      port:    3306, // or 5432 (for postgres)
+    })
 
 app.engine(".html",ejs.__express);
 app.set("view engine", "html");
@@ -16,7 +16,6 @@ app.set("view engine", "html");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
 app.use("/static", express.static("public"));
@@ -26,9 +25,15 @@ app.use("/static", express.static("bower_components"));
 app.get('/', function(req, res) {
     res.render('login');
 })
-
-
-
+//
+var User = sequelize.define('user', {  //创建模型
+      id: Sequelize.INTEGER,
+      username: Sequelize.STRING,
+      password: Sequelize.STRING,
+      email: Sequelize.STRING
+  },{
+  freezeTableName: true // Model tableName will be the same as the model name
+})
 
 var server = app.listen(3000, function () {
 
---
+++ public/javascripts/login.js
@@ -0,0 +1,23 @@
+// $(function() {
+//     $('#submit').on('click', function() {
+//         if(checkRequired()) {
+//             // $.post('/login',{},function(result) {
+//             //
+//             // })
+//         }
+//     })
+//
+// })
+//
+// function checkRequird() {
+//     var flag = true;
+//     $("[placeholder]").each(function(key,val) {
+//         if(val.value ==""){
+//             flag=false;
+//             alert(val.placeholder);
+//         }
+//     })
+//     return flag;
+// }
+console.log($('body'));
+// console.log('asd');
