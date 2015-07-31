39ffde004bb016efd53f127f7b53c2639a1f93ba
maming <864845434@qq.com>
2015-07-31 17:05:27 +0800

    click button go to other page

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,23 +1,6 @@
-// $(function() {
-//     $('#submit').on('click', function() {
-//         if(checkRequired()) {
-//             // $.post('/login',{},function(result) {
-//             //
-//             // })
-//         }
-//     })
-//
-// })
-//
-// function checkRequird() {
-//     var flag = true;
-//     $("[placeholder]").each(function(key,val) {
-//         if(val.value ==""){
-//             flag=false;
-//             alert(val.placeholder);
-//         }
-//     })
-//     return flag;
-// }
-console.log($('body'));
-// console.log('asd');
+$(function() {
+    $('#button').on('click',function() {
+        window.location.href="/register";
+    
+    })
+})
