73279a187729abe334b060c4539453bbb30e81fd
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-07-31 17:08:18 +0800

    modify app.js add the index

--- app.js
+++ app.js
@@ -3,14 +3,17 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var Sequelize = require('sequelize')
-var sequelize = new Sequelize('web_store', 'root', 'root', {
-        host: "localhost",
-      dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
-      port:    3306, // or 5432 (for postgres)
-    })
+var  Sequelize  =  require('sequelize')
+var  sequelize  =  new  Sequelize('web_store',  'root',  'root',   {
+    host: "localhost",
+          dialect:   "mysql",
+      // or 'sqlite', 'postgres', 'mariadb'
+          port:     3306,
+      // or 5432 (for postgres)
+        
+});
 
-app.engine(".html",ejs.__express);
+app.engine(".html", ejs.__express);
 app.set("view engine", "html");
 
 app.use(bodyParser.urlencoded({
@@ -21,28 +24,31 @@ app.use(express.static("bower_components"));
 app.use("/static", express.static("public"));
 app.use("/static", express.static("bower_components"));
 
+app.get('/', function(req, res) {
+    res.render('index', {});
+});
 
 app.get('/register', function(req, res) {
     res.render('register');
 })
 
 app.get('/login', function(req, res) {
-    res.render('login');
+        res.render('login');
+    })
+    //
+var User = sequelize.define('user', { //创建模型
+    id: Sequelize.INTEGER,
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    email: Sequelize.STRING
+}, {
+    freezeTableName: true // Model tableName will be the same as the model name
 })
-//
-var User = sequelize.define('user', {  //创建模型
-      id: Sequelize.INTEGER,
-      username: Sequelize.STRING,
-      password: Sequelize.STRING,
-      email: Sequelize.STRING
-  },{
-  freezeTableName: true // Model tableName will be the same as the model name
-})
-var server = app.listen(3000, function () {
+var server = app.listen(3000, function() {
 
-  var host = server.address().address;
-  var port = server.address().port;
+    var host = server.address().address;
+    var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%s:%s', host, port);
 
 });
