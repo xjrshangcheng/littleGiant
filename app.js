c0cbc865d18372edb3ab56f55309d13cf3f7a091
maming <864845434@qq.com>
2015-08-01 16:53:16 +0800

    resolve conflicts

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ var  Sequelize  =  require('sequelize');
 var jade = require('jade');
 var  sequelize  =  new  Sequelize('web_store',  'root',  'root',   {
     host: "localhost",
-          dialect:   "mysql",
-          port:     3306,
+    dialect:   "mysql",
+    port:     3306,
 });
 
 // app.engine(".html", ejs.__express);
@@ -22,6 +22,16 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
+
+var User = sequelize.define('user', {  //创建模型
+      id: Sequelize.INTEGER,
+      username: Sequelize.STRING,
+      password: Sequelize.STRING,
+      email: Sequelize.STRING
+  },{
+  freezeTableName: true, // Model tableName will be the same as the model name
+  timestamps: false
+})
 
 app.get('/', function(req, res) {
     res.render('index', {});
@@ -39,12 +49,54 @@ app.get('/register', function(req, res) {
     res.render('register');
 });
 
-app.get('/login', function(req, res) {
-    res.render('login');
-});
+
 app.get('/product-details', function(req, res) {
     res.render('product-details', {});
 });
+
+app.get('/login', function(req, res) {
+    res.render('login');
+})
+
+app.post('/loginSubmit',function(req, res) {
+    var inputName = req.body.inputName;
+    var inputPwd = req.body.inputPwd;
+    console.log(req.body);
+    User.findAll().then(function(e) {
+        var exist = false;
+        e.forEach(function(n) {
+            if(n.dataValues.username === inputName && n.dataValues.password === inputPwd) {
+                res.send({
+                    status : 200,
+                    data : 'ok'
+                });
+                exist = true;
+            } else if(n.dataValues.username === inputName && n.dataValues.password !== inputPwd) {
+                res.send({
+                    status : 100,
+                    data : 'pwd_error'
+                });
+                exist = true;
+
+            } else if(n.dataValues.username !== inputName) {
+                exist = false;
+            }
+        })
+         if(!exist) {
+            res.send({
+                status : 100,
+                data : 'username_error'
+            })
+        }
+    })
+})
+
+
+
+
+
+
+
 
 var server = app.listen(3000, function() {
 
--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,9 +1,55 @@
 $(function() {
-    // $('#button').on('click', function() {
-    //     window.location.href="/register";
-    //
-    // })
-    // $('#submit').on('click', function() {
-    //     window.location.href="/";
-    // })
+    $('#submit').on('click', function() {
+    if(checkRequired()) {
+        var inputName = $('#uid').prop('value');
+        var inputPwd = $('#psw1').prop('value');
+        // $.post('/loginSubmit', {
+        //     inputName : inputName,
+        //     inputPwd : inputPwd
+        // }, function(result) {
+        //     if(result.data === 'ok') {
+        //         window.location.href="/";
+        //     }else if(result.data === 'pwd_error') {
+        //         // alert('密码错误');
+        //         $.messager.alert('密码错误')
+        //         // $('#uid').prop('message','密码错误');
+        //     }else if(result.data === 'username_error') {
+        //         alert('用户名不存在');
+        //         // $('#psw1').prop('message','用户名不存在');
+        //     }
+        // })
+        $.ajax({
+            url : '/loginSubmit',
+            type : 'post',
+            data : {
+                inputName : inputName,
+                inputPwd : inputPwd
+            },success : function(result) {
+                if(result.data === 'ok') {
+                       window.location.href="/";
+                   }else if(result.data === 'pwd_error') {
+                       alert('密码错误');
+                                         // $('#uid').prop('message','密码错误');
+                   }else if(result.data === 'username_error') {
+                    //    alert('用户名不存在');
+                    $("#uid").trigger("easyform-ajax", false);
+
+
+                       // $('#psw1').prop('message','用户名不存在');
+                   }
+            }
+        })
+    }
+
+    })
 })
+
+function checkRequired() {
+    var flag = true;
+    $("[placeholder]").each(function(key,val) {
+        if(val.value.length > 16 || val.value.length < 6) {
+            flag = false;
+        }
+    })
+    return flag;
+}
