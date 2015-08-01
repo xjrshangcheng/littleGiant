0f6e9167bcf1b735251b968c8fe7b8be9cbf9a61
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-01 16:44:25 +0800

    fix the view engine to jade

--- app.js
+++ app.js
@@ -3,22 +3,26 @@ var express = require('express');
 var app = express();
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
-var  Sequelize  =  require('sequelize');
-var  sequelize  =  new  Sequelize('web_store',  'root',  'root',   {
+var  Sequelize  =  require('sequelize');
+var jade = require('jade');
+var  sequelize  =  new  Sequelize('web_store',  'root',  'root',   {
     host: "localhost",
-          dialect:   "mysql",
-          port:     3306,    
+          dialect:   "mysql",
+          port:     3306,
 });
 
-app.engine(".html", ejs.__express);
-app.set("view engine", "html");
+// app.engine(".html", ejs.__express);
+// app.set("view engine", "html");
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
+
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
-    
+
 app.get('/', function(req, res) {
     res.render('index', {});
 });
