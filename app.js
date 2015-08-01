6785afcbff37bc99e539788af670b3fbe0122e72
Emon0526 <465293685@qq.com>
2015-08-01 16:28:20 +0800

    Revert 12e0505..430a882
    This rolls back to commit 12e0505b4b2b3a27bb99e31caee6a893c75f0e56.

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
-var jade = require('jade');
+var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var  Sequelize  =  require('sequelize');
+var  sequelize  =  new  Sequelize('web_store',  'root',  'root',   {
+    host: "localhost",
+          dialect:   "mysql",
+          port:     3306,    
+});
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
+app.engine(".html", ejs.__express);
+app.set("view engine", "html");
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
-
+    
 app.get('/', function(req, res) {
     res.render('index', {});
 });
