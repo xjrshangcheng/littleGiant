c5a516ef599f6c561f68dc749b710612db06de9e
Emon0526 <465293685@qq.com>
2015-08-01 16:11:31 +0800

    update app.js

--- app.js
+++ app.js
@@ -1,24 +1,18 @@
 var mysql = require('mysql');
 var express = require('express');
 var app = express();
-var ejs = require('ejs');
+var jade = require('jade');
 var bodyParser = require('body-parser');
-var  Sequelize  =  require('sequelize');
-var  sequelize  =  new  Sequelize('web_store',  'root',  'root',   {
-    host: "localhost",
-          dialect:   "mysql",
-          port:     3306,    
-});
 
-app.engine(".html", ejs.__express);
-app.set("view engine", "html");
+app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "jade");
 
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
-    
+
 app.get('/', function(req, res) {
     res.render('index', {});
 });
