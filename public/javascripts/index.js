5315a54393117130db28cb5103c2697104fe3a02
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-03 15:40:46 +0800

    by getElementByid to find and add the picture from backstage

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,8 @@
-function getInfo() {
+$(function() {
     $.get('/slides-picture', function(data) {
-        document.getElementById('slides-picture').src = data.dataPath;
-
+        for (var i = 0; i < data.dataPath.length; i++) {
+            console.log('slides-picture-' + (i+1));
+            document.getElementById('slides-picture-' + (i+1)).src = data.dataPath[i];
+        }
     });
-}
+});
