052b46c39184aeea54f4dc0fd5d430759663f5f8
Emon0526 <465293685@qq.com>
2015-08-03 11:19:07 +0800

    Complete registration to add data to the user table

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var mysql = require('mysql');
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var Sequelize  =  require('sequelize');
-var sequelize  =  new  Sequelize('little_giant',  'twer',  'twer',   {
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('little_giant', 'twer', 'twer', {
     host: "192.168.10.110",
-    dialect:   "mysql",
-    port:     3306,
+        dialect:   "mysql",
+        port:     3306,
 });
 
 app.set('views', __dirname + '/views');
@@ -19,14 +19,14 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 app.use(express.static("bower_components"));
 
-var User = sequelize.define('user', {  //创建模型
-      id: Sequelize.INTEGER,
-      username: Sequelize.STRING,
-      password: Sequelize.STRING,
-      email: Sequelize.STRING
-  },{
-  freezeTableName: true, // Model tableName will be the same as the model name
-  timestamps: false
+var User = sequelize.define('user', { //创建模型
+    id: Sequelize.INTEGER,
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    email: Sequelize.STRING
+}, {
+    freezeTableName: true, // Model tableName will be the same as the model name
+    timestamps: false
 })
 
 app.get('/', function(req, res) {
@@ -37,7 +37,7 @@ app.get("/car", function(req, res) {
     res.render("car", {});
 })
 
-app.get("/shoppingCart",function(req, res) {
+app.get("/shoppingCart", function(req, res) {
     res.render("shoppingCart", {});
 })
 
@@ -58,39 +58,52 @@ app.get('/login', function(req, res) {
     res.render('login');
 })
 
-app.post('/loginSubmit',function(req, res) {
+app.post('/loginSubmit', function(req, res) {
     var inputName = req.body.inputName;
     var inputPwd = req.body.inputPwd;
     console.log(req.body);
     User.findAll().then(function(e) {
         var exist = false;
         e.forEach(function(n) {
-            if(n.dataValues.username === inputName && n.dataValues.password === inputPwd) {
+            if (n.dataValues.username === inputName && n.dataValues.password === inputPwd) {
                 res.send({
-                    status : 200,
-                    data : 'ok'
+                    status: 200,
+                    data: 'ok'
                 });
                 exist = true;
-            } else if(n.dataValues.username === inputName && n.dataValues.password !== inputPwd) {
+            } else if (n.dataValues.username === inputName && n.dataValues.password !== inputPwd) {
                 res.send({
-                    status : 100,
-                    data : 'pwd_error'
+                    status: 100,
+                    data: 'pwd_error'
                 });
                 exist = true;
 
-            } else if(n.dataValues.username !== inputName) {
+            } else if (n.dataValues.username !== inputName) {
                 exist = false;
             }
         })
-         if(!exist) {
+        if (!exist) {
             res.send({
-                status : 100,
-                data : 'username_error'
+                status: 100,
+                data: 'username_error'
             })
         }
     })
 })
 
+
+app.post('/registerSubmit', function(req, res) {
+
+    var inputName = req.body.inputName;
+    var inputPwd = req.body.inputPwd;
+    var inputEmail = req.body.inputEmail;
+    console.log(req.body);
+    User.create({
+        username : inputName,
+        password : inputPwd,
+        email : inputEmail
+    })
+})
 var server = app.listen(3000, function() {
 
     var host = server.address().address;
--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,5 +1,28 @@
 $(function() {
-    // $('#register_button').on('click',function() {
-    //     window.location.href="/login";
-    // })
+    $('#register').on('click',function() {
+        var inputName = $('#uid').prop('value');
+        var inputPwd = $('#psw1').prop('value');
+        var inputEmail = $('#email').prop('value');
+        $.ajax({
+            url : '/registerSubmit',
+            type : 'post',
+            data : {
+                inputName : inputName,
+                inputPwd : inputPwd,
+                inputEmail : inputEmail
+            }
+            // ,success : function(result) {
+            //     if(result.data === 'ok') {
+            //            window.location.href="/";
+            //        }else if(result.data === 'pwd_error') {
+            //            alert('密码错误');
+            //         // $('#uid').prop('message','密码错误');
+            //        }else if(result.data === 'username_error') {
+            //            alert('用户名不存在');
+            //         // $("#uid").trigger("easyform-ajax", false);
+            //            // $('#psw1').prop('message','用户名不存在');
+            //        }
+            // }
+        })
+    })
 })
