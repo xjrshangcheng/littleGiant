b329c277089f6f472be57769f287becc1f51500f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-03 12:52:03 +0800

    create and add index.js

---
+++ public/javascripts/index.js
@@ -0,0 +1,8 @@
+$(function() {
+    app.get('/slides-picture', function(req, res) {
+        Goods.findAll().then(function() {
+            var picture-path = []
+        });
+    });
+    document.getElementById('slides-picture').src = " ";
+});
