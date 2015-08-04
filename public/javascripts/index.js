f076689cc09cd8911f64d0852ebdcf3cc5689954
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-04 13:21:40 +0800

    delete the index.js

--- public/javascripts/index.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $.get('/slidesPicture', function(data) {
-        for (var i = 0; i < data.dataPath.length; i++) {
-            console.log('slides-picture-' + (i+1));
-            document.getElementById('slides-picture-' + (i+1)).src = data.dataPath[i];
-        }
-    });
-});
