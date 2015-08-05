7a4e54a3349ee83caf2488b26f90574f45e3c946
wanglitao <wanglitao1202@outlook.com>
2015-08-05 16:36:58 +0800

    提起app.js

---
+++ router/shopping-cart.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../models/index');
+var sequelize = db.sequelize;
+var Sequelize = db.Sequelize;
+
+var User = sequelize.define('user', {
+    id: Sequelize.INTEGER,
+    username: Sequelize.STRING,
+    password: Sequelize.STRING,
+    email: Sequelize.STRING
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+
+var Goods = sequelize.define('goods', {
+    id: Sequelize.INTEGER,
+    name: Sequelize.STRING,
+    info: Sequelize.STRING,
+    price: Sequelize.STRING,
+    standard_one: Sequelize.STRING,
+    standard_two: Sequelize.STRING,
+    sales: Sequelize.STRING,
+    detall: Sequelize.INTEGER,
+    type: Sequelize.STRING,
+    img: Sequelize.STRING,
+    recommend: Sequelize.STRING
+}, {
+    freezeTableName: true,
+    timestamps: false
+})
+
+var user_shopping_cart = sequelize.define('user_shopping_cart',{
+    id : Sequelize.INTEGER,
+    username : Sequelize.STRING,
+    number : Sequelize.INTEGER,
+    name : Sequelize.STRING,
+    price : Sequelize.STRING
+}, {
+    freezeTableName : true,
+    timestamps : false
+})
+
+
+router.get("/shopping-cart", function(req, res) {
+   res.render("shopping-cart");
+});
+
+router.get("/shopping-cart-item", function(req, res) {
+   user_shopping_cart.findAll().then(function(data) {
+       var array = [];
+
+       for (var i = 0; i < data.length; i++) {
+           array.push(data[i].dataValues);
+       }
+       res.send({data:array});
+   });
+});
+
+module.exports = router;
