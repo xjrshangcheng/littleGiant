d1f62bd4e8594e346283891dfae295fd21c48e4f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-05 10:34:17 +0800

    add logic code

--- router/index.js
+++ router/index.js
@@ -1,8 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models/index')
 
-router.get('/', function(req, res) {
-  res.render('index');
+var Goods = db.sequelize.define('goods', {
+    id: db.Sequelize.INTEGER,
+    name: db.Sequelize.STRING,
+    info: db.Sequelize.STRING,
+    price: db.Sequelize.STRING,
+    standard_one: db.Sequelize.STRING,
+    standard_two: db.Sequelize.STRING,
+    sales: db.Sequelize.STRING,
+    detall: db.Sequelize.INTEGER,
+    type: db.Sequelize.STRING,
+    img: db.Sequelize.STRING,
+    recommend: db.Sequelize.STRING
+},{
+    freezeTableName : true,
+    timestamps : false
 });
 
+router.get('/', function(req, res) {
+    var goods = [];
+    Goods.findAll().then(function(good) {
+        for (var i = 0; i < good.length; i++) {
+            if (good[i].dataValues.recommend === 'true') {
+                goods.push(good[i].dataValues);
+            }
+        }
+    }).then(function() {
+        res.render('index');
+    });
+});
 module.exports = router;
