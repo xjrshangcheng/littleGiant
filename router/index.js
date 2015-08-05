45df8294cef208cc464657cecd7004daf3b9c284
Lucky-LengYi <meeting_love_lengyi@hotmail.com>
2015-08-05 14:09:23 +0800

    add the function to find databases

--- router/index.js
+++ router/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/index')
+var db = require('../models/index');
 var sequelize = db.sequelize;
 var Sequelize = db.Sequelize;
 
@@ -16,21 +16,55 @@ var Goods = sequelize.define('goods', {
     type: Sequelize.STRING,
     img: Sequelize.STRING,
     recommend: Sequelize.STRING
-},{
-    freezeTableName : true,
-    timestamps : false
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+
+var category = sequelize.define('category', {
+    id: Sequelize.INTEGER,
+    parent_id: Sequelize.INTEGER,
+    name: Sequelize.STRING,
+    path: Sequelize.STRING,
+}, {
+    freezeTableName: true,
+    timestamps: false
 });
 
 router.get('/', function(req, res) {
+    var navigation = [];
     var goods = [];
-    Goods.findAll().then(function(good) {
-        for (var i = 0; i < good.length; i++) {
-            if (good[i].dataValues.recommend !== 'false') {
-                goods.push(good[i].dataValues);
+    category.findAll().then(function(nav) {
+        var primaryClassification = [];
+        var secondaryClassification = [];
+        for (var i = 0; i < nav.length; i++) {
+            if (nav[i].dataValues.parent_id === 0) {
+                primaryClassification.push(nav[i].dataValues);
+            } else if (nav[i].dataValues.parent_id === 2) {
+                secondaryClassification.push(nav[i].dataValues);
             }
         }
+        navigation.push({
+            primaryClassification: primaryClassification,
+            secondaryClassification: secondaryClassification
+        });
     }).then(function() {
-        res.render('index');
+        Goods.findAll().then(function(good) {
+            var roll = [];
+            for (var i = 0; i < good.length; i++) {
+                if (good[i].dataValues.recommend === 'roll') {
+                    roll.push(good[i].dataValues);
+                }
+            }
+            goods.push({
+                roll: roll
+            });
+        }).then(function() {
+            res.render('index', {
+                navigation: navigation,
+                goods: goods
+            });
+        });
     });
 });
 module.exports = router;
