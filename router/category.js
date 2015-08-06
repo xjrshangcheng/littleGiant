2fab56c1c22fe32004f8067ca1d27f7dd375a68e
LiuYutian <l709178159@gmail.com>
2015-08-07 00:46:23 +0800

    get url get database in category info

--- router/category.js
+++ router/category.js
@@ -18,13 +18,42 @@ var Goods = sequelize.define('goods', {
     timestamps: false
 });
 
+var category = sequelize.define('category', {
+    id: Sequelize.INTEGER,
+    parent_id: Sequelize.INTEGER,
+    name: Sequelize.STRING,
+    path: Sequelize.STRING,
+}, {
+    freezeTableName: true,
+    timestamps: false
+});
+
 router.get('/category', function(req, res) {
+    var categoryArray = [];
+
+    category.find({
+        where: {
+            name: req.query.type
+        }
+    }).then(function(categoryObj) {
+        categoryObj.dataValues.path.split(".").forEach(function(n) {
+            category.find({
+                where: {
+                    id: n
+                }
+            }).then(function(categoryInfoObj) {
+                categoryArray.unshift(categoryInfoObj.dataValues.name);
+            });
+        });
+    }).done(function() {
+        console.log(categoryArray);
+    });
+
+
     Goods.findAll().then(function(result) {
         var resultArray = result.map(function(object) {
             return object.dataValues;
         })
-
-        console.log(resultArray);
 
         res.render("category", {
             data: resultArray
