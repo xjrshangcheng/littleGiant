d3e614efded187c8032324718686f59d1cea3d77
wanglitao <wanglitao1202@outlook.com>
2015-08-06 17:58:24 +0800

    Do change the function of the number

--- public/javascripts/shopping-cart.js
+++ public/javascripts/shopping-cart.js
@@ -1,4 +1,49 @@
+
+var print = function() {
+    $.get('/shopping-cart-item', function(items_information) {
+        $('.shopping_cart_goods').empty();
+        console.log(items_information.data);
+        items_information.data.forEach(function(value) {
+            $('<ul class="buycart_content cart_goods" goods_id="24124" id="cart_goods">'
+                    +'<li class="buycart_content_table1">'
+                        +'<input type="checkbox" class="add_goods_tobuy_choose_0" name="add_goods_tobuy_choose_0" id="add_goods_tobuy_24124">'
+                    +'</li>'
+                    +'<li class="buycart_content_table2">'
+                        +'<a href="/product-details">'
+                            +'<img src="./images/goods-detail-picture/ym2.jpg" >'
+                        +'</a>'
+                    +'</li>'
+                    +'<li class="buycart_content_table3">'
+                        +'<a href="/product-details">'
+                            +'<input class="goods_name" type="text" name="name" value="'+ value.name +'" readonly>'
+                        +'</a>'
+                    +'</li>'
+                    +'<li class="buycart_content_table4">'
+                        +'￥<input class="price" type="text" name="price" value="'+ value.price +'" readonly>'
+                    +'</li>'
+                    +'<li class="buycart_content_table5">'
+                        +'<input class="changes'+value.id+'" type="button" name="add" value="-">'
+                        +'<input id="cart_goods_count" class="cart_goods_count'+value.id+'" name="number" type="text" value="'+ value.number +'">'
+                        +'<input class="changes'+value.id+'" type="button" name="reduction" value="+">'
+                    +'</li>'
+                    +'<li class="buycart_content_table6">'
+                        +'￥<input class="subtotal" type="test" name="name" value="'+(value.price) * (value.number)+'" readonly>'
+                    +'</li>'
+                    +'<li class="buycart_content_table7">'
+                        +'<a class="cart_goods_delete">删除</a>'
+                    +'</li>'
+                +'</ul>'
+            ).appendTo($('.shopping_cart_goods'));
+        });
+        a();
+    });
+};
+print();
+
+var a = function() {
+
 $(function() {
+
     $(':input[name = all-check]').on('click', function() {
         var checked = $(this).prop('checked');
         $(':input[name=add_goods_tobuy_choose_0]').prop('checked', checked);
@@ -27,6 +72,7 @@ $(function() {
         $(':input[class = total]').prop('value', subtotal);
     });
 
+
     $(':input[class=cart_goods_count]').on('input propertychange',function() {
         var price = $(':input[class=price]').prop('value');
         var number = $(':input[class=cart_goods_count]').prop('value');
@@ -40,46 +86,7 @@ $(function() {
     });
 });
 
-var print = function() {
-    $.get('/shopping-cart-item', function(items_information) {
-        $('.shopping_cart_goods').empty();
-        items_information.data.forEach(function(value) {
-            console.log(value);
-            $('<ul class="buycart_content cart_goods" goods_id="24124" id="cart_goods">'
-                    +'<li class="buycart_content_table1">'
-                        +'<input type="checkbox" class="add_goods_tobuy_choose_0" name="add_goods_tobuy_choose_0" id="add_goods_tobuy_24124">'
-                    +'</li>'
-                    +'<li class="buycart_content_table2">'
-                        +'<a href="/product-details">'
-                            +'<img src="./images/goods-detail-picture/ym2.jpg" >'
-                        +'</a>'
-                    +'</li>'
-                    +'<li class="buycart_content_table3">'
-                        +'<a href="/product-details">'
-                            +'<input class="goods_name" type="text" name="name" value="'+ value.name +'" readonly>'
-                        +'</a>'
-                    +'</li>'
-                    +'<li class="buycart_content_table4">'
-                        +'￥<input class="price" type="text" name="price" value="'+ value.price +'" readonly>'
-                    +'</li>'
-                    +'<li class="buycart_content_table5">'
-                        +'<input class="changes" type="button" name="add" value="-">'
-                        +'<input id="cart_goods_count" class="cart_goods_count" name="number" type="text" value="'+ value.number +'">'
-                        +'<input class="changes" type="button" name="reduction" value="+">'
-                    +'</li>'
-                    +'<li class="buycart_content_table6">'
-                        +'￥<input class="subtotal" type="test" name="name" value="'+(value.price) * (value.number)+'" readonly>'
-                    +'</li>'
-                    +'<li class="buycart_content_table7">'
-                        +'<a class="cart_goods_delete">删除</a>'
-                    +'</li>'
-                +'</ul>'
-            ).appendTo($('.shopping_cart_goods'));
-        });
-    });
-};
-print();
-
+}
 $("#cart_goods_count").on("keydown", function(evt) {
     var NUM1 = 57;
     var NUM2 = 48;
