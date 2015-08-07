48715cd1aa3cc5072476ba0a8afae1d6329bf08e
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 11:53:00 +0800

    extract the use model to models

---
+++ models/user.js
@@ -0,0 +1,14 @@
+var sequelize = require('../db-connection/connection');
+
+module.exports = function(sequelize, DataTypes) {
+    var User = sequelize.define('user', {
+        id: DataTypes.INTEGER,
+        username: DataTypes.STRING,
+        password: DataTypes.STRING,
+        email: DataTypes.STRING
+    }, {
+        freezeTableName: true,
+        timestamps: false
+    });
+    return User;
+}
