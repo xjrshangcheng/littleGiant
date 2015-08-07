8614e92afbff69186ee8af70819646a005fec88b
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 09:52:06 +0800

    add thumbnail logic code in add-shopping-cart js file

--- public/javascripts/add-shopping-cart.js
+++ public/javascripts/add-shopping-cart.js
@@ -14,17 +14,17 @@ $("#add_to_cart").on("click", function() {
     var price = $("#sales_price").html();
     var cookieName = getCookie('name');
 
-    if(cookieName === null) {
-        $(location).attr('href','/login');
+    if (cookieName === null) {
+        $(location).attr('href', '/login');
     } else {
         $.post("/add-user-shopping-cart", {
-            id : id,
-            username : username,
-            number : number,
-            name : name,
-            price : price
+            id: id,
+            username: username,
+            number: number,
+            name: name,
+            price: price
         }, function(data) {
-            if(data.message === "success") {
+            if (data.message === "success") {
                 alert("添加购物车成功");
             } else {
                 alert("添加数据失败");
@@ -34,17 +34,17 @@ $("#add_to_cart").on("click", function() {
 })
 
 
-$("#goods_number_minus").on("click",function() {
+$("#goods_number_minus").on("click", function() {
     var number = $("#goods_number_input")[0].value;
 
-    if (number <=2) {
+    if (number <= 2) {
         $("#goods_number_input")[0].value = 1;
     } else {
         $("#goods_number_input")[0].value = number - 1;
     }
 })
 
-$("#goods_number_plus").on("click",function() {
+$("#goods_number_plus").on("click", function() {
     var number = $("#goods_number_input")[0].value;
 
     $("#goods_number_input")[0].value = parseInt(number) + 1;
@@ -59,10 +59,13 @@ $("#goods_number_input").on("keydown", function(evt) {
     var NUM6 = 37;
     var NUM7 = 39;
 
-    if(!(evt.keyCode <= NUM1 && evt.keyCode >= NUM2
-        || evt.keyCode === NUM3 || evt.keyCode === NUM4
-        || evt.keyCode === NUM5 || evt.keyCode === NUM6
-        || evt.keyCode === NUM7)) {
+    if (!(evt.keyCode <= NUM1 && evt.keyCode >= NUM2 || evt.keyCode === NUM3 || evt.keyCode === NUM4 || evt.keyCode === NUM5 || evt.keyCode === NUM6 || evt.keyCode === NUM7)) {
         evt.preventDefault();
     }
+});
+
+
+$(".picture").on('mousemove', function() {
+    var src = this.src;
+    $(".big-picture").prop("src", src);
 });
