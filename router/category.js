83cc8733d63f0e42f257e8bdbae101f5dd4e0c0f
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 11:48:37 +0800

    extract the category model from category js

--- router/category.js
+++ router/category.js
@@ -1,32 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/index')
-var sequelize = db.sequelize;
-var Sequelize = db.Sequelize;
-
-var Goods = sequelize.define('goods', {
-    id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    info: Sequelize.STRING,
-    price: Sequelize.STRING,
-    sales: Sequelize.STRING,
-    detail: Sequelize.STRING,
-    type: Sequelize.STRING,
-    img: Sequelize.STRING
-}, {
-    freezeTableName: true,
-    timestamps: false
-});
-
-var Category = sequelize.define('category', {
-    id: Sequelize.INTEGER,
-    parent_id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    path: Sequelize.STRING,
-}, {
-    freezeTableName: true,
-    timestamps: false
-});
+var models = require('../models');
+var Goods = models.goods;
+var Category = models.category;
 
 router.get('/category', function(req, res) {
     var breadArray = [];
