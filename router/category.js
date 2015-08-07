dddc75d641e5d30ebebc726bd456e368705cebb8
LiuYutian <l709178159@gmail.com>
2015-08-07 17:04:49 +0800

    finish goods view

--- router/category.js
+++ router/category.js
@@ -10,39 +10,43 @@ router.get('/category', function(req, res) {
     var sunCategory = [];
     req.query.type = req.query.type || "所有分类";
 
-        Category.findAll().then(function(object) {
-            var selectId;
-            var parentId;
-            object.forEach(function(data) {
-                if (data.dataValues.name === req.query.type) {
-                    data.dataValues.path.split(".").forEach(function(id) {
-                        object.forEach(function(data) {
-                            if (data.dataValues.id == id) {
-                                breadArray.push(data.dataValues.name);
-                            }
-                        });
+    Category.findAll().then(function(object) {
+        var selectId;
+        var parentId;
+        object.forEach(function(data) {
+            if (data.dataValues.name === req.query.type) {
+                data.dataValues.path.split(".").forEach(function(id) {
+                    object.forEach(function(data) {
+                        if (data.dataValues.id == id) {
+                            breadArray.push(data.dataValues.name);
+                        }
                     });
-                }
+                });
+            }
 
-                if (data.dataValues.name === req.query.type) {
-                    selectId = data.dataValues.id;
-                    parentId = data.dataValues.parent_id;
-                }
-            });
-
-            object.forEach(function(data) {
-                if (data.dataValues.path.indexOf(selectId + ".") !== -1 && data.dataValues.parent_id === parentId + 1) {
-                    sunCategory.push(data.dataValues.name);
-                }else if(req.query.type === "所有分类" && data.dataValues.parent_id === 0){
-                    sunCategory.push(data.dataValues.name);
-                }
-            })
+            if (data.dataValues.name === req.query.type) {
+                selectId = data.dataValues.id;
+                parentId = data.dataValues.parent_id;
+            }
         });
 
-    Goods.findAll().then(function(result) {
+        object.forEach(function(data) {
+            if (data.dataValues.path.indexOf(selectId + ".") !== -1 && data.dataValues.parent_id === parentId + 1) {
+                sunCategory.push(data.dataValues.name);
+            } else if (req.query.type === "所有分类" && data.dataValues.parent_id === 0) {
+                sunCategory.push(data.dataValues.name);
+            }
+        })
+    });
+
+    Goods.findAll({
+        where: {
+            type: req.query.type
+        }
+    }).then(function(result) {
         var resultArray = result.map(function(object) {
             return object.dataValues;
-        })
+        });
 
         res.render("category", {
             data: resultArray,
