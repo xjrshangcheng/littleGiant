7e6f4138662114fb80a50964e3f51483b3466384
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 11:57:31 +0800

    extract the user and goods model

--- router/shopping-cart.js
+++ router/shopping-cart.js
@@ -1,35 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/index');
-var sequelize = db.sequelize;
-var Sequelize = db.Sequelize;
-
-var User = sequelize.define('user', {
-    id: Sequelize.INTEGER,
-    username: Sequelize.STRING,
-    password: Sequelize.STRING,
-    email: Sequelize.STRING
-}, {
-    freezeTableName: true,
-    timestamps: false
-});
-
-var Goods = sequelize.define('goods', {
-    id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    info: Sequelize.STRING,
-    price: Sequelize.STRING,
-    standard_one: Sequelize.STRING,
-    standard_two: Sequelize.STRING,
-    sales: Sequelize.STRING,
-    detail: Sequelize.STRING,
-    type: Sequelize.STRING,
-    img: Sequelize.STRING,
-    recommend: Sequelize.STRING
-}, {
-    freezeTableName: true,
-    timestamps: false
-});
+var models = require('../models');
+var User = models.user;
+var Goods = models.goods;
 
 var user_shopping_cart = sequelize.define('user_shopping_cart', {
     id: Sequelize.INTEGER,
