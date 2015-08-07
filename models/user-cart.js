dc906fe2cf51d8f9d6132248972c2f82b914f5fa
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 13:58:19 +0800

    write the user-cart model

---
+++ models/user-cart.js
@@ -0,0 +1,15 @@
+var sequelize = require('../db-connection/connection');
+
+module.exports = function(sequelize, DataTypes) {
+    var UserCart = sequelize.define('user-shopping-cart',{
+        id : DataTypes.INTEGER,
+        username : DataTypes.STRING,
+        number : DataTypes.INTEGER,
+        name : DataTypes.STRING,
+        price : DataTypes.STRING
+    }, {
+        freezeTableName : true,
+        timestamps : false
+    })
+    return UserCart;
+}
