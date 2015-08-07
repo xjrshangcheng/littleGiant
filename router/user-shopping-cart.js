7ee872843c2b461870b3f4163eeadb88ba783ebb
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 14:28:14 +0800

    change the model's name from usershoppingcart to cart

--- router/user-shopping-cart.js
+++ router/user-shopping-cart.js
@@ -1,18 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var models = require('../models');
-var UserCart = models.UserCart;
+var Cart = models.cart;
 
 router.post("/",function(req,res) {
 
-    UserCart.findAll({
+    Cart.findAll({
         where : {
             id : req.body.id,
             username : req.body.username
         }
     }).then(function(data) {
         if(data[0] === undefined) {
-            UserCart.create({
+            Cart.create({
                id : req.body.id,
                username : req.body.username,
                number : req.body.number,
@@ -20,7 +20,7 @@ router.post("/",function(req,res) {
                price : req.body.price
            })
        } else {
-            UserCart.update({
+            Cart.update({
                 number : parseInt(data[0].number) + parseInt(req.body.number)
             }, {
                 where : {
