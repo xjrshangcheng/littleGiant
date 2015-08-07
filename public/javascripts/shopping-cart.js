06382d14568a5b4377a3e42155b204d74e12865e
wanglitao <wanglitao1202@outlook.com>
2015-08-07 11:35:43 +0800

    forget to submit the code

--- public/javascripts/shopping-cart.js
+++ public/javascripts/shopping-cart.js
@@ -1,43 +1,46 @@
-$(':input[name = all-check]').on('click', function() {
-    var checked = $(this).prop('checked');
-    $(':input[name=add_goods_tobuy_choose_0]').prop('checked', checked);
-});
+$(function() {
 
-$(':input[name = add_goods_tobuy_choose_0]').on('click', function() {
-    var allChecked = $(":input[name=add_goods_tobuy_choose_0]:checked").length === $('input[name=add_goods_tobuy_choose_0]').length;
-    $(":input[name=all-check]").prop("checked", allChecked);
-});
+    $(':input[name = all-check]').on('click', function() {
+        var checked = $(this).prop('checked');
+        $(':input[name=add_goods_tobuy_choose_0]').prop('checked', checked);
+    });
+    $(':input[name = add_goods_tobuy_choose_0]').on('click', function() {
+        var allChecked = $(":input[name=add_goods_tobuy_choose_0]:checked").length === $('input[name=add_goods_tobuy_choose_0]').length;
+        $(":input[name=all-check]").prop("checked", allChecked);
+    });
 
-$(':input[class = changes]').on('click', function() {
-    var changes = $(this).prop('value');
-    var number = $(':input[class = cart-goods-count]').prop('value')*1;
-    if (changes === '-') {
-        number = number - 1;
-    }
-    if (changes === '+') {
-        number = number + 1;
-    }
-    if (number<=0) {
-        number=1;
-    }
-    $(':input[class = cart-goods-count]').prop('value', number);
-    var price = $(':input[class=price]').prop('value');
-    var subtotal = price *number;
-    $(':input[class = subtotal]').prop('value', subtotal);
-    $(':input[class = total]').prop('value', subtotal);
-});
+    $(':input[class = changes]').on('click', function() {
+        var changes = $(this).prop('value');
+        var number = $(':input[class = cart-goods-count]').prop('value')*1;
+        console.log(number);
+        if (changes === '-') {
+            number = number - 1;
+        }
+        if (changes === '+') {
+            number = number + 1;
+        }
+        if (number<=0) {
+            number=1;
+        }
+        $(':input[class = cart-goods-count]').prop('value', number);
+        var price = $(':input[class=price]').prop('value');
+        var subtotal = price *number;
+        $(':input[class = subtotal]').prop('value', subtotal);
+        $(':input[class = total]').prop('value', subtotal);
+    });
 
 
-$(':input[class=cart_goods_count]').on('input propertychange',function() {
-    var price = $(':input[class=price]').prop('value');
-    var number = $(':input[class=cart_goods_count]').prop('value');
-    if (number<=0) {
-        number=1;
-    }
-    $(':input[class = cart_goods_count]').prop('value', number);
-    var subtotal = price * number;
-    $(':input[class = subtotal]').prop('value', subtotal);
-    $(':input[class = total]').prop('value', subtotal);
+    $(':input[class=cart_goods_count]').on('input propertychange',function() {
+        var price = $(':input[class=price]').prop('value');
+        var number = $(':input[class=cart_goods_count]').prop('value');
+        if (number<=0) {
+            number=1;
+        }
+        $(':input[class = cart_goods_count]').prop('value', number);
+        var subtotal = price * number;
+        $(':input[class = subtotal]').prop('value', subtotal);
+        $(':input[class = total]').prop('value', subtotal);
+    });
 });
 
 
@@ -57,3 +60,18 @@ $("#cart_goods_count").on("keydown", function(evt) {
         evt.preventDefault();
     }
 });
+
+
+// $(function() {
+//    getInfo();
+//    $('#table').on('click', function(evt) {
+//        $.ajax({
+//            url: '/student_name',
+//            type: 'delete',
+//            data: {
+//                id: evt.target.id
+//            }
+//        });
+//        getInfo();
+//    });
+// });
