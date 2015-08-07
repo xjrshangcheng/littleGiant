f52fe9541d6e4b57c844bf1a1b1cc000aa0b047b
Emon0526 <465293685@qq.com>
2015-08-07 09:50:44 +0800

    Modify registration successful jump address

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -8,7 +8,7 @@ $(function() {
                 inputEmail: $('#email').prop('value')
             }, function() {
                 alert('注册成功，赶快登陆开始购物吧！')
-                window.location.href = "/login";
+                window.location.href = "/";
             })
         })
     }
@@ -17,13 +17,14 @@ $(function() {
 function validation() {
 
     var inputName = $("#uid").val();
+    var isExisted;
     $.post('/name', {
         inputName: inputName
-    }, function(result) {
-        if (result.data === 'user_exist') {
-            var isExisted = false;
+    }, function(message) {
+        if (message.data === 'user_exist') {
+            isExisted = false;
         } else {
-            var isExisted = true;
+            isExisted = true;
         }
         $("#uid").trigger("easyform-ajax", isExisted);
     })
