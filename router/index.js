68732c444c05ac88bc124a5deca33e12b3213f19
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-07 11:37:59 +0800

    extract the goods and category to models file

--- router/index.js
+++ router/index.js
@@ -1,35 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../models/index');
-var sequelize = db.sequelize;
-var Sequelize = db.Sequelize;
-
-var Goods = sequelize.define('goods', {
-    id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    info: Sequelize.STRING,
-    price: Sequelize.STRING,
-    standard_one: Sequelize.STRING,
-    standard_two: Sequelize.STRING,
-    sales: Sequelize.STRING,
-    detail: Sequelize.STRING,
-    type: Sequelize.STRING,
-    img: Sequelize.STRING,
-    recommend: Sequelize.STRING
-}, {
-    freezeTableName: true,
-    timestamps: false
-});
-
-var Category = sequelize.define('category', {
-    id: Sequelize.INTEGER,
-    parent_id: Sequelize.INTEGER,
-    name: Sequelize.STRING,
-    path: Sequelize.STRING,
-}, {
-    freezeTableName: true,
-    timestamps: false
-});
+var models = require('../models');
+var Goods = models.goods;
+var Category = models.category;
 
 router.get('/', function(req, res) {
     var navigation = {};
