9b2acd8fcfb8dae50d71a9fa30e8a5c4c8293084
luojinghui <939035243@qq.com>
2015-08-10 14:16:37 +0800

    alter files product.css add-cart.js and jade function name

--- public/javascripts/add-shopping-cart.js
+++ public/javascripts/add-shopping-cart.js
@@ -7,11 +7,11 @@ function getCookie(name) {
 }
 
 $("#add_to_cart").on("click", function() {
-    var id = $("#goods_code").html();
+    var id = $("#goods-code").html();
     var username = getCookie('name');
-    var number = parseInt($("#goods_number_input").prop("value")) === 0 ? 1 : $("#goods_number_input").prop("value");
-    var name = $(".buy_content_text_a1").html();
-    var price = $("#sales_price").html();
+    var number = parseInt($("#goods-number-input").prop("value")) === 0 ? 1 : $("#goods-number-input").prop("value");
+    var name = $(".buy-content-text-a1").html();
+    var price = $("#sales-price").html();
     var cookieName = getCookie('name');
 
     if (cookieName === null) {
@@ -35,22 +35,22 @@ $("#add_to_cart").on("click", function() {
 
 
 $("#goods_number_minus").on("click", function() {
-    var number = $("#goods_number_input")[0].value;
+    var number = $("#goods-number-input")[0].value;
 
     if (number <= 2) {
-        $("#goods_number_input")[0].value = 1;
+        $("#goods-number-input")[0].value = 1;
     } else {
-        $("#goods_number_input")[0].value = number - 1;
+        $("#goods-number-input")[0].value = number - 1;
     }
 })
 
 $("#goods_number_plus").on("click", function() {
-    var number = $("#goods_number_input")[0].value;
+    var number = $("#goods-number-input")[0].value;
 
-    $("#goods_number_input")[0].value = parseInt(number) + 1;
+    $("#goods-number-input")[0].value = parseInt(number) + 1;
 })
 
-$("#goods_number_input").on("keydown", function(evt) {
+$("#goods-number-input").on("keydown", function(evt) {
     var NUM1 = 57;
     var NUM2 = 48;
     var NUM3 = 8;
