42929a7b646c36e7ba80cb6ea5b5591b15446ceb
luojinghui <939035243@qq.com>
2015-08-11 16:46:18 +0800

    bring logic function remove to service file

--- router/add.js
+++ router/add.js
@@ -2,41 +2,10 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 var Cart = models.cart;
+var add = require('../service/add');
 
 router.post("/",function(req,res) {
-
-    Cart.findAll({
-        where : {
-            id : req.body.id,
-            username : req.body.username
-        }
-    }).then(function(data) {
-        if(data[0] === undefined) {
-            Cart.create({
-               id : req.body.id,
-               username : req.body.username,
-               number : req.body.number,
-               name : req.body.name,
-               price : req.body.price
-           })
-       } else {
-            Cart.update({
-                number : parseInt(data[0].number) + parseInt(req.body.number)
-            }, {
-                where : {
-                    id : req.body.id,
-                    username : req.body.username
-                }
-            })
-       }
-   }).done(function() {
-        res.send({
-            status : 100,
-            data : "",
-            message : "success"
-        })
-        res.end();
-    })
+    add(req,res);
 })
 
 module.exports = router;
---
+++ service/add.js
@@ -0,0 +1,38 @@
+var models = require('../models');
+var Cart = models.cart;
+
+var add = function(req, res) {
+    Cart.findAll({
+        where : {
+            id : req.body.id,
+            username : req.body.username
+        }
+    }).then(function(data) {
+        if(data[0] === undefined) {
+            Cart.create({
+               id : req.body.id,
+               username : req.body.username,
+               number : req.body.number,
+               name : req.body.name,
+               price : req.body.price
+           })
+       } else {
+            Cart.update({
+                number : parseInt(data[0].number) + parseInt(req.body.number)
+            }, {
+                where : {
+                    id : req.body.id,
+                    username : req.body.username
+                }
+            })
+       }
+   }).done(function() {
+        res.send({
+            status : 100,
+            data : "",
+            message : "success"
+        })
+        res.end();
+    })
+};
+module.exports = add;
