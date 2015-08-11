8257f0eb38691d08315927b5be7ffff6a0812073
luojinghui <939035243@qq.com>
2015-08-11 11:17:36 +0800

    alter table goods word type

--- migrations/20150804134143-unnamed-migration.js
+++ migrations/20150804134143-unnamed-migration.js
@@ -33,7 +33,8 @@ module.exports = {
                 img: DataTypes.STRING,
                 recommend: DataTypes.STRING,
                 more_img: DataTypes.STRING,
-                special_img: DataTypes.STRING
+                special_img: DataTypes.STRING,
+                detail_img: DataTypes.STRING
             }
         );
 
