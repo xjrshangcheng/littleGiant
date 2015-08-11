cadfc8a30cf3def71f9d85443f141e18e8405614
luojinghui <939035243@qq.com>
2015-08-11 16:46:45 +0800

    bring logic goods function remove to service file

--- router/goods.js
+++ router/goods.js
@@ -2,33 +2,11 @@ var express = require('express');
 var router = express.Router();
 var models = require('../models');
 var Goods = models.goods;
+var goods = require('../service/goods');
 
 router.get('/', function(req, res) {
     var id = req.query.id;
-    var goodsData = [];
-    var imgDate;
-    var imgDetail;
-
-    Goods.findAll({
-        where : {
-            id : id
-        }
-    }).done(function(data) {
-        if(data[0] === undefined) {
-            res.render('error',{})
-        } else {
-            data.forEach(function(val) {
-                goodsData.push(val.dataValues);
-                imgDate = val.dataValues.more_img === null ? [] : val.dataValues.more_img.split(" ");
-                imgDetail = val.dataValues.detail_img === null ? [] : val.dataValues.detail_img.split(" ");
-            })
-            res.render('goods', {
-                goodsData : goodsData,
-                imgDate : imgDate,
-                imgDetail : imgDetail
-            });
-        }
-    })
+    goods(req,res,id);
 });
 
 module.exports = router;
---
+++ service/goods.js
@@ -0,0 +1,30 @@
+var models = require('../models');
+var Goods = models.goods;
+
+var goods = function(req, res,id) {
+    var goodsData = [];
+    var imgDate;
+    var imgDetail;
+
+    Goods.findAll({
+        where : {
+            id : id
+        }
+    }).done(function(data) {
+        if(data[0] === undefined) {
+            res.render('error',{})
+        } else {
+            data.forEach(function(val) {
+                goodsData.push(val.dataValues);
+                imgDate = val.dataValues.more_img === null ? [] : val.dataValues.more_img.split(" ");
+                imgDetail = val.dataValues.detail_img === null ? [] : val.dataValues.detail_img.split(" ");
+            })
+            res.render('goods', {
+                goodsData : goodsData,
+                imgDate : imgDate,
+                imgDetail : imgDetail
+            });
+        }
+    })
+};
+module.exports = goods;
