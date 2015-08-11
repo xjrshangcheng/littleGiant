63490189de40f14227d76c518fd75702401aa5dc
maming <864845434@qq.com>
2015-08-11 16:10:35 +0800

    beautify service folder login.js file

--- service/login.js
+++ service/login.js
@@ -1,27 +1,29 @@
 var models = require('../models');
 var User = models.user;
-var login =  function(req, res,inputName,inputPwd) {
+var login = function(req, res, inputName, inputPwd) {
     var result;
     var status;
 
     User.findAll({
-        where : {
-            username : inputName
+        where: {
+            username: inputName
         }
     }).then(function(data) {
-            data.forEach(function(val) {
-                if(val.dataValues.password === inputPwd) {
-                    result = 'ok';
-                    status = 200;
-                    res.cookie('name', inputName, { expires: new Date(Date.now() + 1000*60*60)});
-                }
-            })
+        if (data.length > 0) {
+            if (data[0].dataValues.password === inputPwd) {
+                result = 'ok';
+                status = 200;
+                res.cookie('name', inputName, {
+                    expires: new Date(Date.now() + 1000 * 60 * 60)
+                });
+            }
+        }
     }).done(function() {
         res.send({
-           status : status,
-           data : result,
-           message : ''
-       })
+            status: status,
+            data: result,
+            message: ''
+        })
     })
 };
 module.exports = login;
