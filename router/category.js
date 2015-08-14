63049e9a7fe4994cc71cb8bf6bd1f3c2f5512e5d
LiuYutian <l709178159@gmail.com>
2015-08-14 23:12:26 +0800

    page display, the current page number display, each page shows the number of complete

--- router/category.js
+++ router/category.js
@@ -3,32 +3,144 @@ var router = express.Router();
 var models = require('../models');
 var Goods = models.goods;
 var Category = models.category;
-
-var ALL_CATEGORY = "所有分类";
+var nowPage = 0;
+var pageSize = 8;
 
 router.get('/', function(req, res) {
-    var resultGoods = [];
     var breadcrumbs = [];
-    var resultSubCategories = [];
     var subCategories = [];
-    var sumPage;
-    var nowPage = 0;
-    var pageSize = 1;
+    var subAllCategory = [];
+    var count = 3;
+    nowPage = 0;
 
-    Goods.count().then(function(i){
-        sumPage = Math.ceil(i / pageSize);
+    getBreadcrumbs(req, function(data) {
+        breadcrumbs.push("所有分类");
+        data.forEach(function(n) {
+            breadcrumbs.push(n);
+        });
+        count--;
+        if(count === 0) {
+            getSumPage(subAllCategory, pageSize, function(sum) {
+                getGoodsAndResponse(subAllCategory, breadcrumbs, subCategories, sum, res);
+            });
+        }
     });
 
-    breadcrumbs.push(ALL_CATEGORY);
-    req.query.type = req.query.type || ALL_CATEGORY;
+    getSubCategories(req, function(data) {
+        subCategories = data;
+        count--;
+        if(count === 0) {
+            getSumPage(subAllCategory, pageSize, function(sum) {
+                getGoodsAndResponse(subAllCategory, breadcrumbs, subCategories, sum, res);
+            });
+        }
+    });
 
-    // 生成breacrumbs
+    getSubAllCategories(req, function(data) {
+        subAllCategory = data;
+        count--;
+        if(count === 0) {
+            getSumPage(subAllCategory, pageSize, function(sum) {
+                getGoodsAndResponse(subAllCategory, breadcrumbs, subCategories, sum, res);
+            });
+        }
+    });
+});
+
+function getSumPage(subAllCategory, pageSize, setSumpage) {
+    Goods.count({
+        where : {
+            type : subAllCategory
+        }
+    }).then(function(count){
+        setSumpage(Math.ceil(count / pageSize));
+    });
+}
+
+function getGoodsAndResponse(subAllCategory, breadcrumbs, subCategories, sumPage, res) {
+    getGoodsInfo(subAllCategory, function(data) {
+        if(data.length > 0) {
+            nowPage++;
+        }
+        res.render("category", {
+            data:data,
+            breadArray:breadcrumbs,
+            subCategory:subCategories,
+            sumPage:sumPage,
+            nowPage:nowPage
+        })
+    });
+}
+
+function getGoodsInfo(subAllCategory, setGoodsInfo) {
+    Goods.findAll({
+        where : {
+            type : subAllCategory
+        },
+        limit:[nowPage*pageSize, pageSize]
+    }).then(function(result) {
+        resultGoods = result.map(function(categoryRecord) {
+            return categoryRecord.dataValues;
+        });
+
+        setGoodsInfo(resultGoods);
+    });
+}
+
+function getSubAllCategories(req, setSubAllCategory) {
+    var subAllCategory = [];
+    Category.find({
+        where: {
+            name:req.query.type
+        }
+    }).then(function(resultCurrentCategory) {
+        Category.findAll({
+            where: {
+                path: {
+                    $like: resultCurrentCategory.dataValues.path + "%"
+                }
+            }
+        }).then(function(resultCategory) {
+            resultCategory.forEach(function(data) {
+                subAllCategory.push(data.dataValues.name);
+            });
+            setSubAllCategory(subAllCategory);
+        });
+    });
+}
+
+function getSubCategories(req, setSubCategory) {
+    var resultSubCategories = [];
+
+    Category.find({
+        where: {
+            name:req.query.type
+        }
+    }).then(function(resultCurrentCategory) {
+        Category.findAll({
+            where: {
+                path: {
+                    $like: resultCurrentCategory.dataValues.path + "%"
+                },
+                level : parseInt(resultCurrentCategory.dataValues.level) + 1
+            }
+        }).then(function(resultCategory) {
+            resultCategory.forEach(function(data) {
+                resultSubCategories.push(data.dataValues.name);
+            });
+            setSubCategory(resultSubCategories);
+        });
+    });
+}
+
+function getBreadcrumbs(req, setBreadcrumbs) {
+    var breadcrumbs = [];
     Category.find({
         where: {
             name: req.query.type
         }
     }).then(function(resultCurrentCategory) {
-        if(resultCurrentCategory !== null){
+        if (resultCurrentCategory !== null) {
             Category.findAll({
                 where: {
                     id: resultCurrentCategory.dataValues.path.split(".")
@@ -37,78 +149,9 @@ router.get('/', function(req, res) {
                 resultCategoryIds.forEach(function(resultCategory) {
                     breadcrumbs.push(resultCategory.dataValues.name);
                 });
+                setBreadcrumbs(breadcrumbs);
             });
         }
-    });
-
-    // 生成resultSubCategories
-    Category.find({
-        where: {
-            name:req.query.type
-        }
-    }).then(function(resultCurrentCategory) {
-        Category.findAll({
-            where: {
-                path: {
-                    $like: resultCurrentCategory.dataValues.path+"%"
-                },
-                level:resultCurrentCategory.dataValues.level+1
-            }
-        }).then(function(resultCategory) {
-            resultCategory.forEach(function(data) {
-                resultSubCategories.push(data.dataValues.name);
-            });
-        });
-    });
-
-    // 生成subCategories
-    Category.find({
-        where: {
-            name:req.query.type
-        }
-    }).then(function(resultCurrentCategory) {
-        Category.findAll({
-            where: {
-                path: {
-                    $like: resultCurrentCategory.dataValues.path+"%"
-                }
-            }
-        }).then(function(resultCategory) {
-            resultCategory.forEach(function(data) {
-                subCategories.push(data.dataValues.name);
-            });
-
-            if (req.query.type !== ALL_CATEGORY) {
-                var whereObj = {
-                    where: {
-                        type: subCategories
-                    },
-                    limit: [nowPage*pageSize, pageSize]
-                };
-
-                findGoods(res, resultGoods, breadcrumbs, resultSubCategories, nowPage + 1, sumPage, whereObj);
-            } else {
-                findGoods(res, resultGoods, breadcrumbs, resultSubCategories, nowPage + 1, sumPage, undefined);
-            }
-
-            nowPage++;
-        });
-    });
-});
-
-function findGoods(res, resultGoods, breadcrumbs, resultSubCategories, nowPage, sumPage, whereObj) {
-    Goods.findAll(whereObj).then(function(result) {
-        resultGoods = result.map(function(categoryRecord) {
-            return categoryRecord.dataValues;
-        });
-
-        res.render("category", {
-            data: resultGoods,
-            breadArray: breadcrumbs,
-            subCategory: resultSubCategories,
-            nowPage : nowPage,
-            sumPage : sumPage
-        })
     });
 }
 
