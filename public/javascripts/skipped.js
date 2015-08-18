7bb7f0501ac22e336eaa68e1087b097092f5aeba
wengjiaojiao <wengjiaojiao@outlook.com>
2015-08-18 13:50:30 +0800

    create the skipped js file

---
+++ public/javascripts/skipped.js
@@ -0,0 +1,15 @@
+$(function() {
+    var count = 5;
+    var newSpan = document.createElement("span");
+
+    document.getElementById('count-down').appendChild(newSpan);
+    $("#count-down span").animate({height:"10px",width:"169px"});
+
+    window.setInterval(function() {
+        newSpan.innerHTML = count + '秒后跳转到登陆页面。';
+        count--;
+        if (count === -1) {
+            location.href = "./login";
+        }
+    }, 1000);
+});
