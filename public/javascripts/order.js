71e685d596fbd633edfab76677a213b97fb49cbf
Emon0526 <465293685@qq.com>
2015-08-21 14:55:58 +0800

    Change the about box again

--- public/javascripts/order.js
+++ public/javascripts/order.js
@@ -11,26 +11,24 @@ $(function() {
 function statusChange(event) {
     var $current = $(this);
     var id = $(this).data('id');
-    if (confirm("已经确定收货了吗?")) {
+    $('#goods').on('click', function() {
         $.post("/order/alterStatus", {
             id: id
         }, function() {
             $('div[data-id=' + id + '].trading').html('交易成功');
             $('div[data-id=' + id + '].confirm').html('已确认收货');
         });
-    }
+    })
 }
 
 function orderDelete(event) {
     var $current = $(this);
     var id = $(this).data('id');
-    $('#deleteit').on('click', function(){
-
+    $('#deleteit').on('click', function() {
         $.post("/order/delete", {
             id: id
         }, function() {
             $current.closest('tbody').remove();
         });
     })
-
 }
