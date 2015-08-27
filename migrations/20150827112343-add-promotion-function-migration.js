9550ab3aa9a186c509a6dcfe5fc23c7e73d4c0a9
luojinghui <939035243@qq.com>
2015-08-27 16:58:21 +0800

    add table promotion

---
+++ migrations/20150827112343-add-promotion-function-migration.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: function (migration, DataTypes) {
+    migration.createTable(
+        'promotion', {
+          id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+          },
+          type: DataTypes.STRING,
+          rule: DataTypes.STRING,
+          add_time: DataTypes.DATE,
+          start_time: DataTypes.DATE,
+          end_time: DataTypes.DATE,
+          status: DataTypes.STRING
+        }
+    );
+
+    migration.addColumn(
+        'goods',
+        'promotion',
+        DataTypes.STRING
+    )
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+  }
+};
