1c2351363c918265d9f77c8f293d30144df76019
luojinghui <939035243@qq.com>
2015-08-28 10:13:35 +0800

    alter model promotion

--- models/promotion.js
+++ models/promotion.js
@@ -1,8 +1,11 @@
 var sequelize = require('../db-connection/connection');
 
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
     var Promotion = sequelize.define('promotion', {
-        id: DataTypes.INTEGER,
+        id: {
+            type:DataTypes.INTEGER,
+            primaryKey: true
+        },
         type: DataTypes.STRING,
         rule: DataTypes.STRING,
         add_time: DataTypes.DATE,
@@ -11,7 +14,16 @@ module.exports = function(sequelize, DataTypes) {
         status: DataTypes.STRING
     }, {
         freezeTableName: true,
-        timestamps: false
+        timestamps: false,
+        classMethods: {
+            associate: function(model) {
+                Promotion.hasOne(model.goods, {
+                    foreignKey: {
+                        name: 'promotion'
+                    }
+                });
+            }
+        }
     });
     return Promotion;
 };
