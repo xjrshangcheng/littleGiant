f266c0d575a6baa084cabc98e1d96722df3cc3f3
wanglitao <wanglitao1202@outlook.com>
2015-08-28 16:15:06 +0800

    wlt clean code

--- service/shopping-cart.js
+++ service/shopping-cart.js
@@ -22,32 +22,32 @@ var shoppingCart = function(req, res, userName) {
                 id: idStore
             }
         }).then(function(data) {
-                data.forEach(function(goodsId, i) {
-                    idStore.forEach(function(cartId) {
-                        if (cartId === goodsId.dataValues.id) {
-                            allInformation[i].img = goodsId.dataValues.img;
-                        }
-                    });
-                });
-                Promotion.findAll({
-                    where: {
-                        id: idStore
+            data.forEach(function(goodsId, i) {
+                idStore.forEach(function(cartId) {
+                    if (cartId === goodsId.dataValues.id) {
+                        allInformation[i].img = goodsId.dataValues.img;
                     }
-                }).then(function(data) {
-                    data.forEach(function(promotion,i) {
-                        idStore.forEach(function(cartId) {
-                            if(cartId === promotion.dataValues.id) {
-                                allInformation[i].type = promotion.dataValues.type;
-                            }
-                        })
+                });
+            });
+            Promotion.findAll({
+                where: {
+                    id: idStore
+                }
+            }).then(function(data) {
+                data.forEach(function(promotion, i) {
+                    idStore.forEach(function(cartId) {
+                        if (cartId === promotion.dataValues.id) {
+                            allInformation[i].type = promotion.dataValues.type;
+                        }
                     })
-                        res.render("shopping-cart", {
-                            data: allInformation,
-                            status: 200,
-                            message: ''
-                        });
                 })
+                res.render("shopping-cart", {
+                    data: allInformation,
+                    status: 200,
+                    message: ''
+                });
             })
+        })
     });
 };
 var shoppingCartDelete = function(req, res, userName, Id) {
