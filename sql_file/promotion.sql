d7c5c61e2c14e9f79bc874266ae6911814a0a90b
luojinghui <939035243@qq.com>
2015-08-28 10:58:59 +0800

    add promotion test data

---
+++ sql_file/promotion.sql
@@ -0,0 +1,9 @@
+INSERT INTO `promotion` (`id`, `type`, `rule`, `add_time`, `start_time`, `end_time`, `status`)
+VALUES
+	(1, '直降', '直降99元', NULL, NULL, NULL, '开启'),
+	(2, '直降', '直降88元', NULL, NULL, NULL, '开启'),
+	(3, '满减', '满99减10元', NULL, NULL, NULL, '开启'),
+	(4, '满送', '满10件送一件', NULL, NULL, NULL, '开启'),
+	(5, '直降', '直降88元', NULL, NULL, NULL, NULL),
+	(6, '团购', '满三人减44元', NULL, NULL, NULL, NULL),
+	(7, '打折', '秋季新品5折', NULL, NULL, NULL, NULL);
