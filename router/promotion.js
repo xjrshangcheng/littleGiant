b4344d54e042511f618f8d0feb92491c509388aa
luojinghui <939035243@qq.com>
2015-08-28 10:40:15 +0800

    alter router function

--- router/promotion.js
+++ router/promotion.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var Promotion = require('../controller/promotion-controller');
+var pro = new Promotion();
 
-router.get('/', function(req, res) {
-    res.render('promotion');
-});
+router.get('/', pro.renderPromotion);
+
+router.get('/:promotionId', pro.lablePromotion)
 
 module.exports = router;
---
+++ router/user-shopping-cart.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var router = express.Router();
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('little_giant', 'twer', 'twer', {
+    host: "localhost",
+    dialect:"mysql",
+    port:3306
+})
+var user_shopping_cart = sequelize.define('user_shopping_cart',{
+    id : Sequelize.INTEGER,
+    username : Sequelize.STRING,
+    number : Sequelize.INTEGER,
+    name : Sequelize.STRING,
+    price : Sequelize.STRING
+}, {
+    freezeTableName : true,
+    timestamps : false
+})
+
+router.get('/product-details', function(req, res) {
+    res.render('product-details', {});
+});
+
+router.post("/add_user_shopping_cart",function(req,res) {
+    var id = req.body.id;
+    var username = req.body.username;
+    var number = req.body.number;
+    var name = req.body.name;
+    var price = req.body.price;
+    console.log(req.body);
+
+    user_shopping_cart.create({
+        id : id,
+        username : username,
+        number : number,
+        name : name,
+        price : price
+    }).then(function() {
+        res.end();
+    })
+})
+
+module.exports = router;
