c6f4f00e4c1551095c83d1c3ebcb4bea9bba269b
luojinghui <939035243@qq.com>
2015-09-01 21:06:20 +0800

    alter promotion page pageCount and goods number

--- controller/promotion-controller.js
+++ controller/promotion-controller.js
@@ -1,49 +1,71 @@
 var models = require('../models');
 var Goods = models.goods;
 var Pro = models.promotion;
-var Promotion = function() {}
+var Promotion = function () {}
 
-Promotion.prototype.renderPromotion = function(req, res) {
+Promotion.renderPromotion = function (req, res) {
     var promotionGoods = [];
     var promotionTypes = [];
+    var pageSize = 10;
+    var pageCount;
+    var currentPage = 0;
 
-    Goods.findAll().then(function(data) {
-        for(var i = 0; i < data.length; i ++) {
-            if(typeof(data[i].dataValues.promotion) === "number") {
-                promotionGoods.push(data[i].dataValues);
+    Goods.findAll({
+        where: {
+            promotion: {
+                gt: 0
             }
-        }
-    }).then(function() {
-    Pro.findAll().then(function(data) {
-        data.forEach(function(val, index) {
-            promotionTypes.push(val.dataValues);
+        },
+        limit: [0, pageSize]
+    }).then(function (data) {
+        return data.forEach(function (val) {
+            promotionGoods.push(val.dataValues);
         })
+    }).then(function () {
+        return Pro.findAll().then(function (data) {
+            data.forEach(function (val, index) {
+                promotionTypes.push(val.dataValues);
+            })
+        })
+    }).then(function() {
+        return Goods.count({
+            where: {
+                promotion: {
+                    gt: 0
+                }
+            }
+        }).then(function (val) {
+            pageCount = Math.ceil(val/pageSize);
+        })
+    }).then(function () {
         res.render("promotion", {
             promotionGoods: promotionGoods,
-            promotionTypes: promotionTypes
-            });
-        })
+            promotionTypes: promotionTypes,
+            pageCount: pageCount
+        });
     })
+    console.log(promotionGoods);
 }
 
-Promotion.prototype.lablePromotion = function(req, res) {
+Promotion.lablePromotion = function (req, res) {
     var id = Number(req.params.promotionId);
 
     Goods.findAll({
         where: {
             promotion: id
         }
-    }).then(function(data) {
-        return data.map(function(val) {
+    }).then(function (data) {
+        return data.map(function (val) {
             return val.dataValues;
         })
-    }).done(function(data) {
+    }).done(function (data) {
         res.send({
             status: 200,
             data: data
         })
     })
 }
+
 
 module.exports = Promotion;
 
